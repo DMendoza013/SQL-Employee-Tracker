34d8801359c7b57e07019ddd59ccf252616a903e
DMendoza013 <damien.mendoza013@gmail.com>
2024-05-21 17:32:15 -0700

    added to server.js

--- server.js
+++ server.js
@@ -1,1 +1,19 @@
 let inquirer = require('inquirer');
+
+const options = [
+    {
+        type: 'list',
+        name: 'choice',
+        message: 'What would you like to do?',
+        choices: ['View All Employees', 'Add Employee', 'Update Employee Role', 'View All Roles', 'Add Role', 'View All Departments', 'Add Department']
+    },
+]
+
+function init() {
+    inquirer.prompt(options)
+    .then(function(data){
+        console.log(data);
+    })
+}
+
+init();
