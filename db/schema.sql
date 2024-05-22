a440bfed359fcfa49515030dc6e958ba9917a0df
DMendoza013 <damien.mendoza013@gmail.com>
2024-05-22 14:16:02 -0700

    added express and pg into server.js and added comments

--- db/schema.sql
+++ db/schema.sql
@@ -1,24 +1,28 @@
+-- creating new table called employee_db;
 DROP DATABASE IF EXISTS employee_db;
 CREATE DATABASE employee_db;
-
-/c employee_db;
-
+-- move into the new database
+\c employee_db;
+-- create a new table called department
 CREATE TABLE department (
     id SERIAL PRIMARY KEY,
     department_name VARCHAR(30) NOT NULL
 );
-
+-- create a new table called role
 CREATE TABLE role (
     id SERIAL PRIMARY KEY,
     title VARCHAR(30) UNIQUE NOT NULL,
     salary DECIMAL NOT NULL,
     department_id INTEGER NOT NULL
-)
-
+);
+-- create a new table called employees
 CREATE TABLE employees (
     id SERIAL PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INTEGER NOT NULL,
     manager_id INTEGER
-)
+);
+
+-- SELECT * FROM <table name> to view a certain table
+-- use \dt; to view current tables
--- server.js
+++ server.js
@@ -1,5 +1,17 @@
-let inquirer = require('inquirer');
+// installing inquirer 
+const inquirer = require('inquirer');
+const express = require('express');
+// creating a connection pool
+const { Pool } = require('pg');
 
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// middleware for POST AND PUT requests, puts incoming request objects in JSON 
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// creating a list of options so that the user can interact with the database
 const options = [
     {
         type: 'list',
@@ -8,12 +20,14 @@ const options = [
         choices: ['View All Employees', 'Add Employee', 'Update Employee Role', 'View All Roles', 'Add Role', 'View All Departments', 'Add Department']
     },
 ]
-
+// creating an init() function that loads the program on start up 
 function init() {
     inquirer.prompt(options)
     .then(function(data){
         console.log(data);
     })
 }
-
+// calling the initial function 
 init();
+
+//npm start to run the program
